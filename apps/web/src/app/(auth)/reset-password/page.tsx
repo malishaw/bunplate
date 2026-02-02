9e9682b1462ae036b9cbc7735641eb39af1a7718
iamVihanga <vihangarashansilva@gmail.com>
2026-02-03 00:38:43 +0530

    fix: [web] suspense boundary error

--- apps/web/src/app/(auth)/reset-password/page.tsx
+++ apps/web/src/app/(auth)/reset-password/page.tsx
@@ -1,5 +1,10 @@
 import { ResetPasswordForm } from "@/modules/auth/components/reset-password-form";
+import { Suspense } from "react";
 
 export default function SigninPage() {
-  return <ResetPasswordForm />;
+  return (
+    <Suspense>
+      <ResetPasswordForm />
+    </Suspense>
+  );
 }
