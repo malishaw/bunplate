f60259db92ce9b1244f82e168e37a17877109c16
iamVihanga <vihangarashansilva@gmail.com>
2026-02-03 15:18:18 +0530

    add rewrite feature

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/:path*",
-  //       destination: "https://bunplate-api.vercel.app/api/:path*"
-  //     }
-  //   ];
-  // }
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://bunplate-api.vercel.app/api/:path*"
+      }
+    ];
+  }
 };
 
 export default nextConfig;
